6a3ea0e68d0080ca641a66198d867499cb172d8a
jaykamp <jaykamp@uw.edu>
2019-10-25 16:52:35 -0700

    fixed some stuff with the html and tests

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,8 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
+
 
 from lists.views import home_page
 
@@ -13,4 +15,14 @@ class HomePageTest(TestCase):
 	def test_home_page_returns_correct_html(self):
 		request = HttpRequest()
 		response = home_page(request)
-		self.assertEqual(response.content.decode(), expected_html)
+		expected_html = render_to_string('home.html', request=request)
+		self.assertEqual(response.content, expected_html)
+
+	def test_home_page_can_save_a_POST_request(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+
+		response = home_page(request)
+
+		self.assertIn('A new list item', response.content.decode())
