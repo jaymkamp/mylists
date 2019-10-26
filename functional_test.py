16d1a888dfdcb2e47a6d97a0b6eff249a4153358
jaykamp <jaykamp@uw.edu>
2019-10-25 23:56:08 -0700

    Prep before refactoring FT

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 class NewVisitorTest(unittest.TestCase):
 	def setUp(self):
@@ -36,22 +39,41 @@ class NewVisitorTest(unittest.TestCase):
 		# "1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
 
+		#need to wait for page to fully load
+		WebDriverWait(self.browser, 10).until(
+            expected_conditions.text_to_be_present_in_element(
+                (By.ID, 'id_list_table'), 'Buy peacock feathers'))
+
+
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-				any(row.text == '1: Buy peacock feathers' for row in rows),
-				"New to-do item not appear in table"
-		)
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
-		self.fail('Finish the test!')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+
+		#need to wait for page to fully load
+		WebDriverWait(self.browser, 10).until(
+            expected_conditions.text_to_be_present_in_element(
+                (By.ID, 'id_list_table'), 'Use peacock feathers to make a fly'))
+
 		# The page updates again, and now shows both items on her list
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn(
+			'2: Use peacock feathers to make a fly',
+			[row.text for row in rows]
+		)
+
 		# Edith wonders whether the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some
 		# explanatory text to that effect.
 		# She visits that URL - her to-do list is still there.
 		# Satisfied, she goes back to sleep
-
+		self.fail('Finish the test!')
 if __name__ == '__main__':
 	unittest.main(warnings='ignore') 
--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-
+import re
 
 from lists.views import home_page
 
@@ -12,11 +12,22 @@ class HomePageTest(TestCase):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
 
+	#need to remove csrf token before comparing html
+	def remove_csrf(self,html_code):
+		csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+		return re.sub(csrf_regex, '', html_code)
+
+	def assertEqualExceptCSRF(self, html_code1, html_code2):
+		return self.assertEqual(
+			self.remove_csrf(html_code1),
+			self.remove_csrf(html_code2)
+		)
+
 	def test_home_page_returns_correct_html(self):
 		request = HttpRequest()
 		response = home_page(request)
 		expected_html = render_to_string('home.html', request=request)
-		self.assertEqual(response.content, expected_html)
+		self.assertEqualExceptCSRF(response.content.decode(), expected_html)
 
 	def test_home_page_can_save_a_POST_request(self):
 		request = HttpRequest()
@@ -26,3 +37,8 @@ class HomePageTest(TestCase):
 		response = home_page(request)
 
 		self.assertIn('A new list item', response.content.decode())
+		expected_html = render_to_string(
+			'home.html',
+			{'new_item_text': 'A new list item'}
+		)
+		self.assertEqualExceptCSRF(response.content.decode(), expected_html)
