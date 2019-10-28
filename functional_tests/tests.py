d8c937da57beed8fb1031030662769e7fe2b8365
jaykamp <jaykamp@uw.edu>
2019-10-28 15:30:07 -0700

    added ft/tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,14 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
-class NewVisitorTest(unittest.TestCase):
+
+class NewVisitorTest(LiveServerTestCase):
+
 	def setUp(self):
 		self.browser = webdriver.Firefox(executable_path = 'C:\\bin\geckodriver.exe')
 		self.browser.implicitly_wait(3)
@@ -21,7 +24,7 @@ class NewVisitorTest(unittest.TestCase):
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith has heard about a cool new online to-do app. She goes
 	 	# to check out its homepage
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 
 		# She notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
@@ -72,5 +75,3 @@ class NewVisitorTest(unittest.TestCase):
 		# She visits that URL - her to-do list is still there.
 		# Satisfied, she goes back to sleep
 		self.fail('Finish the test!')
-if __name__ == '__main__':
-	unittest.main(warnings='ignore') 
